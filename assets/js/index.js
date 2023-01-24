9a231fbd65c0bc2796d5818277368c98f9215ee3
Ana Ferreira <anapaulaferreira0309@gmail.com>
2023-01-24 16:41:34 -0300

    feat: create function to identify user device

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,12 @@
+checkDevice();
+
 function onScroll() {
   const nav = document.getElementById('navigation');
+  let contentHeight = window.scrollY;
 
-  if (scrollY > 0) {
-    nav.classList.add('nav--scroll');
-  } else {
-    nav.classList.remove('nav--scroll');
-  }
+  scrollY > 0 || contentHeight > 0
+    ? nav.classList.add('nav--scroll')
+    : nav.classList.remove('nav--scroll');
 }
 
 const menubar = document.querySelector('nav .navbar button');
@@ -19,17 +20,46 @@ function expandedMenu() {
   const menu = document.querySelector('nav .menu');
   const menuIcon = document.querySelector('nav .navbar button i');
 
-  body.classList.toggle('menu-expanded');
-  nav.classList.toggle('active');
-  menu.classList.toggle('active');
-  menuIcon.classList.toggle('fa-bars');
-  menuIcon.classList.toggle('fa-xmark');
+  let isMobile = checkDevice();
 
-  if (menubar.ariaLabel === 'Abrir menu') {
-    menubar.ariaLabel = 'Fechar menu';
-    menubar.ariaExpanded = 'true';
-  } else {
-    menubar.ariaLabel = 'Abrir menu';
-    menubar.ariaExpanded = 'false';
+  if (isMobile) {
+    body.classList.toggle('menu-expanded');
+    nav.classList.toggle('active');
+    menu.classList.toggle('active');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-xmark');
+
+    if (menubar.ariaLabel === 'Abrir menu') {
+      menubar.ariaLabel = 'Fechar menu';
+      menubar.ariaExpanded = 'true';
+    } else {
+      menubar.ariaLabel = 'Abrir menu';
+      menubar.ariaExpanded = 'false';
+    }
   }
 }
+
+function checkDevice() {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+ScrollReveal({
+  reset: true,
+}).reveal(
+  `.stats__card, .services__group-cards, .card, #about div, #about img`,
+  {
+    delay: 500,
+  }
+);
